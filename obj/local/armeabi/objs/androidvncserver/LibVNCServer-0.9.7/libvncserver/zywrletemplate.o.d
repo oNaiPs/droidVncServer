8e6a0edab4b76c2b4c6aa87dfaa02c5df9001489
José Pereira <onaips@onaips-VGN-FW51MF-H.(none)>
2011-01-15 11:36:30 +0000

    Clipboard support

--- obj/local/armeabi/objs/androidvncserver/LibVNCServer-0.9.7/libvncserver/zywrletemplate.o.d
+++
@@ -1,2 +0,0 @@
-/home/onaips/Dropbox/projectos/workspace/avnc/obj/local/armeabi/objs/androidvncserver/LibVNCServer-0.9.7/libvncserver/zywrletemplate.o: \
- /home/onaips/Dropbox/projectos/workspace/avnc/jni/vnc/LibVNCServer-0.9.7/libvncserver/zywrletemplate.c
