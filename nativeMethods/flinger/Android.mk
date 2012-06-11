2f078ec1955d9a963a884d6b7783de810d631ae5
JosePereira <onaips@gmail.com>
2012-06-11 20:04:16 +0100

    added flinger grabber method

---
+++ nativeMethods/flinger/Android.mk
@@ -0,0 +1,29 @@
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES = \
+									flinger.cpp
+
+#LOCAL_CFLAGS += -DPLATFORM_SDK_VERSION=$(PLATFORM_SDK_VERSION)
+
+LOCAL_C_INCLUDES +=	\
+										$(LOCAL_PATH) \
+										$(LOCAL_PATH)/..
+
+LOCAL_PRELINK_MODULE:=false #override prelink map
+LOCAL_MODULE:= libdvnc_flinger_sdk$(PLATFORM_SDK_VERSION)
+LOCAL_MODULE_TAGS:= optional
+LOCAL_MODULE_PATH := $(LOCAL_PATH)/../lib
+
+ifeq ($(PLATFORM_SDK_VERSION),9)
+LOCAL_SHARED_LIBRARIES := libsurfaceflinger_client libui libbinder libutils  libcutils #libcrypto libssl libhardware
+else ifeq ($(PLATFORM_SDK_VERSION),10)
+LOCAL_SHARED_LIBRARIES := libsurfaceflinger_client libui libbinder libutils  libcutils #libcrypto libssl libhardware
+else ifeq ($(PLATFORM_SDK_VERSION),15)
+LOCAL_SHARED_LIBRARIES := libgui libui libbinder libcutils
+else
+#add here more sdk versions
+LOCAL_SHARED_LIBRARIES := libgui libui libbinder libcutils
+endif
+
+include $(BUILD_SHARED_LIBRARY)
